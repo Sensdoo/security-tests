b4fcfdff856e62c631926bedf48844830e5e9ea2
Sensdoo <sensibleprogrammer@gmail.com>
2017-11-18 21:37:36 +0300

    example spring-boot security thymeleaf

---
+++ spring-boot-spring-security-thymeleaf-example/src/main/java/com/sens/example/SpringBootWebApplication.java
@@ -0,0 +1,15 @@
+package com.sens.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Engineers on 18.11.2017.
+ */
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
